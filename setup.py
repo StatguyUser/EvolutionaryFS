c00b32128b115cc064baf3c73d3043ce4b602b9e
newbiestatguy <azimulh@gmail.com>
2021-05-14 22:55:55 +0300

    Added code and wheel files

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ else:
 
 setup(
     name='EvolutionaryFS',
-    version='0.0.1',
+    version='0.0.2',
     description='Implementation of evolutionary algorithm for machine learning feature selection',
     long_description=long_description,
     long_description_content_type='text/markdown',  # This is important!
